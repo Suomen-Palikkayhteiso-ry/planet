869b4843618787d77b8a5e6a48e118e285fca862
Asko Soukka <asko.soukka@iki.fi>
2025-12-31 21:48:38 +0200

    feat: Parse media:description for YouTube feeds

--- src/Main.hs
+++ src/Main.hs
@@ -135,7 +135,11 @@ parseItem fc item = do
     title <- getItemTitle item
     link <- getItemLink item
     let date = join $ getItemPublishDate item
-    let desc = getItemDescription item
+    let defaultDesc = getItemDescription item
+    let youtubeMediaDesc = case (item, feedType fc) of
+                            (AtomItem entry, YouTube) -> getMediaDescription entry
+                            _ -> Nothing
+    let desc = youtubeMediaDesc `mplus` defaultDesc
     let thumb = getItemThumbnail item `mplus` (desc >>= extractFirstImage)
     return $ AppItem title link date desc thumb (feedTitle fc) (feedType fc)
 
@@ -214,6 +218,25 @@ getUrlAttr e =
     case filter (\(n, _) -> nameLocalName n == "url") (elementAttributes e) of
         ((_, [ContentText t]):_) -> Just t
         _ -> Nothing
+
+-- Media Description Extraction
+getMediaDescription :: Atom.Entry -> Maybe Text
+getMediaDescription entry =
+  let otherElements = Atom.entryOther entry
+      mediaGroups = filter (\e -> nameLocalName (elementName e) == "group" && nameNamespace (elementName e) == Just "http://search.yahoo.com/mrss/") otherElements
+  in case mediaGroups of
+    (g:_) ->
+      let descriptionElements = filter (\e -> nameLocalName (elementName e) == "description") (elementNodes g >>= nodeToElem)
+      in case descriptionElements of
+        (d:_) -> elementText d
+        _ -> Nothing
+    _ -> Nothing
+  where
+    nodeToElem (NodeElement e) = [e]
+    nodeToElem _ = []
+    elementText e = case elementNodes e of
+      [NodeContent (ContentText t)] -> Just t
+      _ -> Nothing
 
 -- HTML Generation
 generateHtml :: Config -> Messages -> [AppItem] -> UTCTime -> LBS.ByteString
